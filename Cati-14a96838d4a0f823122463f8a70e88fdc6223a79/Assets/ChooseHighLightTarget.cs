4ffc6939019030833465862ce6e72cd91a7018d7
CatarinaFidalgo <59732505+CatarinaFidalgo@users.noreply.github.com>
2021-09-02 14:37:40 +0100

    Calibration of Controllers

--- Cati-14a96838d4a0f823122463f8a70e88fdc6223a79/Assets/ChooseHighLightTarget.cs
+++ Cati-14a96838d4a0f823122463f8a70e88fdc6223a79/Assets/ChooseHighLightTarget.cs
@@ -65,6 +65,8 @@ public class ChooseHighLightTarget : MonoBehaviour
                 
             }
 
+            /*
+
             else if (j >= 5 && j < 12)
             {
                 //102 active
@@ -83,9 +85,10 @@ public class ChooseHighLightTarget : MonoBehaviour
                 p101.GetComponent<HighLight>().radius = 0.0f;
                 p102.GetComponent<HighLight>().radius = 0.0f;
                 p103.GetComponent<HighLight>().radius = 0.05f;
-            }
+            } */
 
-            if (j >= 16)
+            //if (j >= 16) 
+            if (j >= 5)
             {
                  
                 evaluation.localIsDemonstrator = !evaluation.localIsDemonstrator;
---
+++ Cati-14a96838d4a0f823122463f8a70e88fdc6223a79/Assets/ControllerCalibration.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerCalibration : MonoBehaviour
+{
+    public Evaluation evaluation;
+    public OVRCameraRig cam;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (evaluation.machine == MachineType.A)
+            cam.transform.position = new Vector3 (-0.76f , 0f, -0.7f);
+
+        if (evaluation.machine == MachineType.B)
+            cam.transform.position = new Vector3(0.13f, 0f, -0.37f);
+    }
+
+    
+}
